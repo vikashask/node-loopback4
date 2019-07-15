05c46652a7a7c1416b28da29d4c7a543886aa665
vikash <vikashraj144@gmail.com>
2019-07-15 23:39:17 +0530

    mysql added

--- dist/datasources/index.js
+++ dist/datasources/index.js
@@ -9,4 +9,5 @@ function __export(m) {
 Object.defineProperty(exports, "__esModule", { value: true });
 __export(require("./db.datasource"));
 __export(require("./geocoder.datasource"));
+__export(require("./mysql.datasource"));
 //# sourceMappingURL=index.js.map
